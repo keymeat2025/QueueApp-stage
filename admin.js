97b358c3b97a87162bfd65bee5d3c03e2e501f87
keymeat2025 <keymeat2025@gmail.com>
2026-02-04 10:22:53 +0530

    Update admin.js
    finger ui  change minor one

--- admin.js
+++ admin.js
@@ -919,7 +919,7 @@ function showQRPosterModal(rid) {
           <div id="poster-qr"></div>
         </div>
         
-        <div class="poster-scan-me">👇 SCAN ME 👇</div>
+        <div class="poster-scan-me">👆 SCAN ME 👆</div>
         <div class="poster-powered-by">Powered by <strong>www.queueapp.in</strong></div>
       </div>
       
